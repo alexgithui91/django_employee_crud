a19d6659a29fc6714c8f478ebde071f5beb500c1
alexgithui91@gmail.com <alexgithui91@gmail.com>
2022-12-18 20:43:42 +0300

    added whitenoise

--- main/settings.py
+++ main/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "employee.apps.EmployeeConfig",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 MIDDLEWARE = [
