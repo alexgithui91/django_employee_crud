b6eb0fefddac9fce62630a8ce020b1419c62e6dd
alexgithui91@gmail.com <alexgithui91@gmail.com>
2022-12-19 11:21:11 +0300

    test 3

--- main/settings.py
+++ main/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "corsheaders",
     "employee.apps.EmployeeConfig",
 ]
 
@@ -55,7 +54,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
